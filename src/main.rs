c4cfbc18b36374a81592d74e13fde61f247c8464
Divik Chotani <divik.chotani@gmail.com>
2025-07-19 00:48:35 -0700

    created new child processes

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,8 @@
 use structopt::StructOpt;
 use nix::{libc::getuid, unistd};
+use libc;
+use std::{os::unix::process::CommandExt, process::{Command, Child}};
+
 fn main() {
     let opt = Opt::from_args();
     let isRoot = unsafe {
@@ -7,20 +10,49 @@ fn main() {
     };
     
     if (opt.files || opt.network) && !isRoot {
-        println!("To track the network and/or file system you must run as root with sudo");
+        println!("To track the network and/or file system you must run as root with sudo, use -h for help");
         return;
     }
     if !(opt.files || opt.network || opt.system) {
         println!("You must at least pass one flag to output logs, use -h for help");
         return;
     }
+
+    //create the subprocess
+
+    let mut sys = if opt.system {
+        Some(spawnProcess("log", &vec!["stream", "--style", "json", "--info"]))
+    } else {
+        None
+    };
+
+    let mut fs = if opt.files {
+        Some(spawnProcess("sudo", &vec!["fs_usage", "-w", "-f", "filesys"]))
+    } else {
+        None
+    };
+
+    let mut net = if opt.network {
+        Some(spawnProcess("log", &vec!["stream", "--style", "json", "--info"]))
+    } else {
+        None
+    };
+
+   
+
+
+    
 }
 
+fn spawnProcess(command: &str, args: &Vec<&str>) -> Child{
+    let pid: i32;
+
+    
+    let res = Command::new(command).args(args).spawn().expect("Failed to run the command");
+    return res
+}
 #[derive(Debug, StructOpt)]
-#[structopt(name = "logger", help = "Specify what part of the mac to log,
-                                    -s for the system and apps,
-                                    -f for file based system calls
-                                    and -n for network packets")]
+#[structopt(name = "logger", help = "Specify what part of the mac to log, -s for the system and apps, -f for file based system calls, and -n for network packets")]
 struct Opt {
 
     #[structopt(short = "s", long = "system")]
